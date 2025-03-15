47e2f00c42b632bcb8470aedb47de8f53ba5055b
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-14 22:33:56 -0300

    recolvido

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string leet;
+
+Console.WriteLine("---Texto a ser traduzido---");
+leet = Console.ReadLine().ToUpper();
+
+leet = leet
+          .Replace("A", "4")
+          .Replace("E", "3")
+          .Replace("I", "1")
+          .Replace("L", "1")
+          .Replace("O", "0")
+          .Replace("T", "7")
+          .Replace("S", "5")
+;
+
+Console.Write(leet);
